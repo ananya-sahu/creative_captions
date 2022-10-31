970ae5e0a16ec1e995cb1a5e64c31eccafb94916
Ananya Sahu <ananya1sahu@gmail.com>
2022-10-31 13:20:25 -0400

    fixed iter in parse

--- CLIP_prefix_caption/parse_coco.py
+++ CLIP_prefix_caption/parse_coco.py
@@ -20,7 +20,7 @@ def main(clip_model_type: str):
     print("%0d captions loaded from json " % len(data))
     all_embeddings = []
     all_captions = []
-    for i in tqdm(len(data)): #changed len(data) to 100
+    for i in tqdm(range(len(data))): #changed len(data) to 100
         d = data[i]
         img_id = d["image_id"]
         filename = f"./data/coco/train2014/COCO_train2014_{int(img_id):012d}.jpg"
