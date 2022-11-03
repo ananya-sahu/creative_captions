cafbdc3caef4af7064b9b16dadd8e229b910b161
Aditi Patil <aap2205@columbia.edu>
2022-11-02 23:32:52 -0400

    fix name of train cap

--- CLIP_prefix_caption/parse_coco.py
+++ CLIP_prefix_caption/parse_coco.py
@@ -15,7 +15,7 @@ def main(clip_model_type: str):
     clip_model_name = clip_model_type.replace('/', '_')
     out_path = f"./data/coco/oscar_split_{clip_model_name}_train.pkl"
     clip_model, preprocess = clip.load(clip_model_type, device=device, jit=False)
-    with open('./data/coco/annotations/train_caption.json', 'r') as f:
+    with open('./data/coco/train_caption_filtered.json', 'r') as f:
         data = json.load(f)
     print("%0d captions loaded from json " % len(data))
     all_embeddings = []
