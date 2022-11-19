2ccb233bf068fb5200cf5352679c6677206ce56c
Ananya Sahu <ananya1sahu@gmail.com>
2022-11-18 20:57:09 -0500

    changes bertscore library imports

--- CLIP_prefix_caption/pycocoevalcap/bert_score_eval.py
+++ CLIP_prefix_caption/pycocoevalcap/bert_score_eval.py
@@ -1,9 +1,13 @@
-from torchmetrics.text.bert import BERTScore
+#!pip install torch==1.5.1
+#!pip install transformers==3.0.1
+#!pip install bert_score==0.3.4
+
 import statistics
 import json
+from bert_score import score
 
-annFile='/Users/ananyasahu/nlp_project/CLIP_prefix_caption/CLIP_prefix_caption/data/coco/annotations/train_caption_filtered.json'
-resFile = '/Users/ananyasahu/nlp_project/CLIP_prefix_caption/CLIP_prefix_caption/captions_for_metric.json'
+annFile = '/content/train_caption_filtered.json'
+resFile = '/content/captions_for_metric.json'
 
 with open(annFile) as json_file:
     data_ref = json.load(json_file)
@@ -20,10 +24,9 @@ for pred_dict in data_pred:
             refs.append(ref_dict["caption"])
 
 
-bertscore = BERTScore()
-score = bertscore(preds, refs)
-rounded_score = {k: [round(v, 3) for v in vv] for k, vv in score.items()}
 
-print(statistics.mean(rounded_score['f1']))
-print(statistics.mean(rounded_score['precision']))
-print(statistics.mean(rounded_score['recall']))
+P, R, F1 = score(preds, refs, lang="en", verbose=True)
+
+print(f"System level P score: {P.mean():.3f}")
+print(f"System level R score: {R.mean():.3f}")
+print(f"System level F1 score: {F1.mean():.3f}")
